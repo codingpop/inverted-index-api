b271760405989f1dad261909a9af5b7ae77f6a36
Babatunde Adeyemi <tundewrites@gmail.com>
2017-04-28 16:35:02 +0100

    Add one more text

--- tests/inverted-index-spec.js
+++ tests/inverted-index-spec.js
@@ -11,6 +11,9 @@ describe('Inverted Index tests', () => {
     it('should return `true` for a valid JSON file', () => {
       expect(testInvertedIndex.isValid(valid)).toBe(true);
       expect(testInvertedIndex.isValid(malformed)).toBe(true);
+    });
+
+    it('should return `false` for an invalid JSON file', () => {
       expect(testInvertedIndex.isValid(empty)).toBe(false);
       expect(testInvertedIndex.isValid(bad)).toBe(false);
     });
@@ -19,7 +22,23 @@ describe('Inverted Index tests', () => {
   describe('Checks if file is malformed', () => {
     it('should return `true` for malformed JSON file', () => {
       expect(testInvertedIndex.isMalformed(malformed)).toBe(true);
+    });
+
+    it('should return `false` for a valid JSON array', () => {
       expect(testInvertedIndex.isMalformed(valid)).toBe(false);
+    });
+  });
+
+  describe('Checks if text is properly tokenized', () => {
+    const text1 = valid[0].text;
+    const text2 = valid[1].text;
+
+    const validTokens1 = Array.from(text1.toLowerCase().split(' '));
+    const validTokens2 = Array.from(text2.toLowerCase().split(' '));
+
+    it('should return `true` if the tokens come in an array', () => {
+      expect(testInvertedIndex.tokenize(text1)).toContain(validTokens1);
+      expect(testInvertedIndex.tokenize(text2)).toContain(validTokens2);
     });
   });
 });
