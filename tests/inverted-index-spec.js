cccda729a778eda30f2ffc507575ce5b6896ddbd
Babatunde Adeyemi <tundewrites@gmail.com>
2017-04-28 16:54:39 +0100

    Modify my tests to the inverted index methods

--- tests/inverted-index-spec.js
+++ tests/inverted-index-spec.js
@@ -1,7 +1,25 @@
-const myBook = require('../src/inverted-index');
+const InvertedIndex = require('../src/inverted-index');
+const valid = require('../fixtures/valid');
+const malformed = require('../fixtures/malformed');
+const bad = require('../fixtures/bad');
+const empty = require('../fixtures/empty');
 
-describe('Inverted Index tests ', () => {
-  it('should return `0` for an empty JSON array', () => {
-    expect(myBook.validateBook([]).toBe('empty'));
+const testInvertedIndex = new InvertedIndex();
+
+describe('Inverted Index tests', () => {
+  describe('Checks if file is valid', () => {
+    it('should return `true` for a valid JSON file', () => {
+      expect(testInvertedIndex.isValid(valid)).toBe(true);
+      expect(testInvertedIndex.isValid(malformed)).toBe(true);
+      expect(testInvertedIndex.isValid(empty)).toBe(false);
+      expect(testInvertedIndex.isValid(bad)).toBe(false);
+    });
+  });
+
+  describe('Checks if file is malformed', () => {
+    it('should return `true` for malformed JSON file', () => {
+      expect(testInvertedIndex.isMalformed(malformed)).toBe(true);
+      expect(testInvertedIndex.isMalformed(valid)).toBe(false);
+    });
   });
 });
