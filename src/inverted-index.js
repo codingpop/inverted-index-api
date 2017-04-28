3b341a544ba0d599172d4ff0cc302d8f1b81b6b4
Babatunde Adeyemi <tundewrites@gmail.com>
2017-04-28 16:55:44 +0100

    Add InvertedIndex class to pass the tests

---
+++ src/inverted-index.js
@@ -0,0 +1,42 @@
+class InvertedIndex {
+  constructor() {
+    this.indices = {};
+  }
+
+  isValid(jsonFile) {
+    return Array.isArray(jsonFile) && !!jsonFile.length;
+  }
+
+  isMalformed(jsonFile) {
+    let result = false;
+    for (let i = 0; i < jsonFile.length; i += 1) {
+      result = !('title' in jsonFile[i] && 'text' in jsonFile[i]);
+      if (result === true) break;
+    }
+    return result;
+  }
+
+  tokenize(text) {
+    text = new Set(text.toLower().split(' '));
+    return Array.from(text);
+  }
+
+  createIndex(fileName, fileContent) {
+    if (!this.isValid()) {
+      return 'The uploaded file is invalid';
+    }
+
+    if (this.isMalformed()) {
+      return 'The JSON file is malformed';
+    }
+
+    const book1Text = this.tokenize(fileContent[0].text);
+    const book2Text = this.tokenize(fileContent[1].text);
+  }
+
+  searchIndex() {
+    
+  }
+}
+
+module.exports = InvertedIndex;
