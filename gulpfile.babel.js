2512728b9118e12417ead4666295ae749177d29d
Babatunde Adeyemi <tundewrites@gmail.com>
2017-05-08 15:16:50 +0100

    Rewrite Gulp tasks

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,35 +1,40 @@
 import gulp from 'gulp';
 import babel from 'gulp-babel';
+import report from 'gulp-istanbul-report';
 import jasmineNode from 'gulp-jasmine-node';
 import istanbul from 'gulp-babel-istanbul';
 import injectModules from 'gulp-inject-modules';
 import coveralls from 'gulp-coveralls';
-import exit from 'gulp-exit';
 
-gulp.task('default', () =>
+gulp.task('transpile', () =>
 gulp.src(['src/inverted-index.js', 'tests/inverted-index-test.js'])
-.pipe((babel())).pipe(gulp.dest('dist')));
+.pipe(babel()).pipe(gulp.dest('dist')));
 
-gulp.task('run-tests', ['default'], () =>
-gulp.src('tests/inverted-index-test.js')
-.pipe(jasmineNode())
-.pipe(exit())
-);
+gulp.task('test', () =>
+gulp.src('./coverage/coverage.json')
+.pipe(report()));
 
-gulp.task('coverage', () => {
-  gulp.src('src/*.js')
+gulp.task('run-test', ['transpile'], () =>
+gulp.src(['tests/inverted-index-test.js'])
+.pipe(jasmineNode()));
+
+gulp.task('coverage', (cb) => {
+  gulp.src(['src/inverted-index.js'])
     .pipe(istanbul())
     .pipe(istanbul.hookRequire())
     .on('finish', () => {
-      gulp.src('tests/*.js')
+      gulp.src('tests/inverted-index-test.js')
       .pipe(babel())
       .pipe(injectModules())
       .pipe(jasmineNode())
       .pipe(istanbul.writeReports())
-      .pipe(istanbul.enforceThresholds({ thresholds: { global: 30 } }))
-      .on('end', () => {
-        gulp.src('coverage/lcov.info')
-        .pipe(coveralls());
-      });
+      .pipe(istanbul.enforceThresholds({ thresholds: { global: 50 } }))
+      .on('end', cb);
     });
 });
+
+gulp.task('coveralls', ['run-test'], () =>
+gulp.src('coverage/lcov.info')
+.pipe(coveralls()));
+
+gulp.task('default', ['run-test', 'coverage', 'coveralls', 'test']);
