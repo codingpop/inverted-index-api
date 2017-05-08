99d23635eaa307ddb9609557badd6ee0d82a5bf2
Babatunde Adeyemi <tundewrites@gmail.com>
2017-05-08 14:35:00 +0100

    Add Gulp tasks for Coveralls

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,6 +1,9 @@
 import gulp from 'gulp';
 import babel from 'gulp-babel';
 import jasmineNode from 'gulp-jasmine-node';
+import istanbul from 'gulp-babel-istanbul';
+import injectModules from 'gulp-inject-modules';
+import coveralls from 'gulp-coveralls';
 
 gulp.task('default', () =>
 gulp.src(['src/inverted-index.js', 'tests/inverted-index-test.js'])
@@ -10,3 +13,22 @@ gulp.task('test', ['default'], () =>
 gulp.src(['tests/inverted-index-test.js'])
 .pipe(jasmineNode())
 );
+
+gulp.task('coverage', (cb) => {
+  gulp.src('src/inverted-index.js')
+    .pipe(istanbul())
+    .pipe(istanbul.hookRequire())
+    .on('finish', () => {
+      gulp.src('tests/inverted-index-test.js')
+      .pipe(babel())
+      .pipe(injectModules())
+      .pipe(jasmineNode())
+      .pipe(istanbul.writeReports())
+      .pipe(istanbul.enforceThresholds({ thresholds: { global: 70 } }))
+      .on('end', cb);
+    });
+});
+
+gulp.task('coverage', ['test'], () =>
+gulp.src('coverage/lcov.info')
+.pipe(coveralls()));
